d44327f9d1cbf6431401f154d7d6411c1cbbf0c0
Geoffrey Fung <g@gfung.net>
2021-01-14 20:45:12 +1100

    Added mobile styles and fixed Vercel.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,22 +24,22 @@ const stagger = {
 const Home: FunctionalComponent = () => (
   <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
     <motion.div
-      className="flex flex-col justify-center items-start xs:max-w-xs sm:max-w-xl lg:max-w-5xl mx-auto mb-16 mt-64"
+      className="flex flex-col justify-center items-start max-w-xs sm:max-w-xl lg:max-w-5xl mx-auto mb-16 mt-64"
       variants={stagger}
     >
       <motion.h1
-        className="text-3xl font-bold text-gray-800 pb-3"
+        className="text-lg sm:text-3xl font-bold text-gray-800 pb-3"
         variants={fadeIn}
       >
         Hey, I'm
       </motion.h1>
       <motion.h1
-        className="text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primaryLight to-primaryDark pb-5 mb-2"
+        className="text-5xl sm:text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primaryLight to-primaryDark pb-2 mb-2 mobile:mb-1"
         variants={fadeIn}
       >
         Geoffrey
       </motion.h1>
-      <motion.p className="text-gray-700" variants={fadeIn}>
+      <motion.p className="text-xs sm:text-lg text-gray-700" variants={fadeIn}>
         I'm a developer and student from Sydney, 🇦🇺. You've found my personal
         slice of the internet, where I hope to record my personal progress as a
         front-end developer. Make yourself at home. ✌️
