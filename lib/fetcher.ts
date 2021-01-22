0da23f116b6ad137c5d267275447ce504f89b03b
Geoffrey Fung <g@gfung.net>
2021-01-22 21:34:29 +1100

    Added User-Agent to Fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,12 @@
 const fetcher = async (path: string) => {
-  const res = await fetch(new URL(path, process.env.BASE_URL).toString());
+  const res = await fetch(new URL(path, process.env.BASE_URL).toString(), {
+    method: 'GET',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+      Accept: 'application/json; charset=UTF-8'
+    }
+  });
   const data = res.json();
 
   return data;
