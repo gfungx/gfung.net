6fc33515c52ff21782dc5043168cb9c5a3d5df7c
Geoffrey Fung <g@gfung.net>
2021-01-24 11:43:47 +1100

    Swapped Google OAuth for Twitter OAuth and added Guestbook link back

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,20 +4,14 @@ import Providers from 'next-auth/providers';
 
 const options = {
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorizationUrl:
-        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
+    Providers.Twitter({
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!
     }),
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
       scope: 'user:email'
-    }),
-    Providers.Twitter({
-      clientId: process.env.TWITTER_CLIENT_ID!,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET!
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
