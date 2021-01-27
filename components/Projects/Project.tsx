57dc158d1e1f019d4e2bccd9a113d6430db6f60e
Geoffrey Fung <g@gfung.net>
2021-01-27 17:04:43 +1100

    Redisnged Project card

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -7,22 +7,57 @@ const Project: FunctionalComponent<ProjectProps> = ({
   language,
   stargazers_count,
   watchers_count,
-  description
+  description,
+  html_url,
+  homepage
 }) => (
-  <a href={`https://github.com/${full_name}`} target="_blank">
-    <div className="h-44 rounded-lg border border-gray-200 shadow-lg transform hover:scale-105 hover:border-gray-500 hover:shadow-2xl transition duration-200 cursor-pointer pt-6 p-8">
-      <div className="flex justify-between">
-        <h1 className="font-bold text-2xl">{name}</h1>
-        <h1 className="pt-1">
-          {stargazers_count} ⭐ | {watchers_count} 👀
-        </h1>
-      </div>
-      <p className="uppercase text-gray-300 text-xs mt-1">
-        {full_name} | {language}
-      </p>
-      <p className="pt-2">{description}</p>
+  <div className="h-48 rounded-lg border border-gray-200 shadow-lg transform hover:scale-105 hover:border-gray-500 hover:shadow-2xl transition duration-200 pt-6 p-8">
+    <div className="flex justify-between">
+      <h1 className="font-bold text-2xl">{name}</h1>
+      <h1 className="pt-1">
+        {stargazers_count} ⭐ | {watchers_count} 👀
+      </h1>
     </div>
-  </a>
+    <p className="uppercase text-gray-300 text-xs mt-1">
+      {full_name} | {language}
+    </p>
+    <p className="pt-1">{description}</p>
+    <p className="font-bold text-blue-500 absolute bottom-4">
+      <a href={html_url} target="_blank">
+        GitHub{' '}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="inline-block -mt-1"
+        >
+          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+        </svg>
+      </a>{' '}
+      {homepage ? (
+        <>
+          <p className="inline-block">|</p>{' '}
+          <a href={homepage} target="_blank" className="inline-block">
+            Homepage{' '}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="inline-block -mt-1"
+            >
+              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+            </svg>
+          </a>
+        </>
+      ) : null}
+    </p>
+  </div>
 );
 
 export default Project;
--- types/projects.d.ts
+++ types/projects.d.ts
@@ -5,6 +5,8 @@ export type Project = {
   stargazers_count: number;
   watchers_count: number;
   description: string;
+  html_url: string;
+  homepage: string | null;
 };
 
 export type Projects = Project[];
